a65710ef6afc0dfa2c63842874c7f1ac43d72169
wjdals95 <wjdals992@naver.com>
2023-02-08 16:49:12 +0900

    NavLink사용

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 //Link: 말 그대로 링크를 생성하는 역할.
+
+import classes from './MainHeader.module.css'
 
 const MainHeader = () => {
     return (
-        <header>
+        <header className={classes.header}>
             <nav>
                 <ul>
                     <li>
-                        <Link to='/welcome'>Welcome</Link>
+                        <NavLink activeClassName={classes.active} to='/welcome'>Welcome</NavLink>
                     </li>
                     <li>
-                        <Link to='/products'>Products</Link>
+                        <NavLink activeClassName={classes.active} to='/products'>Products</NavLink>
                     </li>
                 </ul>
             </nav>
